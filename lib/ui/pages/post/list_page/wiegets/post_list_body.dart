ad6f3afd9d37d30753d59c401d1543b47030a34c
TaengGyul <mjcho0130@gmail.com>
2025-06-09 12:23:02 +0900

    게시글 목록보기 페이징 직전까지 완료

--- lib/ui/pages/post/list_page/wiegets/post_list_body.dart
+++ lib/ui/pages/post/list_page/wiegets/post_list_body.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/gvm/session_gvm.dart';
 import 'package:flutter_blog/ui/pages/post/detail_page/post_detail_page.dart';
+import 'package:flutter_blog/ui/pages/post/list_page/post_list_vm.dart';
 import 'package:flutter_blog/ui/pages/post/list_page/wiegets/post_list_item.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class PostListBody extends StatelessWidget {
-  const PostListBody({Key? key}) : super(key: key);
-
+class PostListBody extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (_) => PostDetailPage()));
-          },
-          child: PostListItem(),
-        );
-      },
-      separatorBuilder: (context, index) {
-        return const Divider();
-      },
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    PostListModel? model = ref.watch(postListProvider); // init 함수가 종료될 때가지 null 임
+
+    if (model == null) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      return ListView.separated(
+        itemCount: model.posts.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => PostDetailPage()));
+            },
+            child: PostListItem(model.posts[index]), // 이건 전달해야 함
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+      );
+    }
   }
 }
